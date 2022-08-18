2be63a2c78f1d4a430a67209dc13138a39e10a71
Brian Clow <bdclow@umich.edu>
2022-08-18 08:39:48 -0400

    add active cell highlighting for dashboard tbl

--- src/dashboard/run.py
+++ src/dashboard/run.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from dash import html, dcc, Dash, Input, Output
+from dash import html, dcc, Dash, Input, Output, dash_table
 from jupyter_dash import JupyterDash
 import plotly.express as px
 import pandas as pd
@@ -13,16 +13,39 @@ def load_data() -> pd.DataFrame:
     '''
     Gets most recent set of predictions
     Loads into pandas DataFrame
+    Cleans up columns (e.g. one hot cols, extraneous, etc)
     '''
     pred_dir = "data/predictions/"
     pred_files = list(os.scandir(pred_dir))
     pred_files.sort(key=lambda f: f.name)
 
+
     df = pd.read_csv(pred_files.pop().path, index_col=0)
 
+    # UTM Campaign
+    utm = df[df.columns[2:18]]
+    # Video categories
+    categories = df[df.columns[31:70]]
+    watched_length_cols = [col for col in df.columns if "watched_video_length" in col]
+
+    cols_to_drop = list(utm.columns)
+    cols_to_drop.extend(["Prediction", "success"])
+    cols_to_drop.extend(categories.columns)
+    cols_to_drop.extend(watched_length_cols)
+
     # Add color column for predictions > THRESHOLD
-    df["color"] = df.ChurnProb.apply(
+    df["Predicted"] = df.ChurnProb.apply(
         lambda x: "Convert" if x < THRESHOLD else "Churn")
+
+    df.ChurnProb = df.ChurnProb.round(2)
+
+    df = df.drop(columns=cols_to_drop)
+    df["UTM Campaign"] = utm.idxmax(1)
+
+    # Clean up column names
+    df = df.rename(columns={
+        col: col.replace("_", " ").title() 
+        for col in df.columns})
 
     return df
 
@@ -33,62 +56,51 @@ server = app.server
 def make_figure_1():
     return px.scatter(
         new_trialers, 
-        x='ChurnProb', 
-        y="total_watchtime_day_1",
-        facet_row='trial_length_days',
+        x='Churnprob', 
+        y="Total Watchtime Day 1",
+        facet_row='Trial Length Days',
         labels={"trial_length_days": "Length of Trial",
                 "total_watchtime_day_1": "Video watch time day 1", 
-                "ChurnProb": "Probability of Churning"},
+                "Churnprob": "Probability of Churning"},
         color_discrete_sequence=["green", "red"],
-        color='color')
+        color='Predicted')
 
 @app.callback(
-    Output('table-1', 'children'),
+    Output('table-1', 'active_cell'),
     Input('graph-1', 'hoverData'))
-def make_table_1(value):
-    '''
-    Construct HTML table from non-zero data in row selected by hovering
-    '''
+def highlight_table_1(value):
     if value:
         index = value['points'][0]['pointNumber']
-        series = new_trialers.iloc[index]
-        index = series.index.values.tolist()
-        return html.Table(
-            [
-                html.Thead(
-                    html.Tr([html.Th("Item"), html.Th("Value")])
-                ),
-                html.Tbody([
-                    html.Tr([
-                        html.Td(ind),
-                        html.Td(series.loc[ind]),
-                    ]) for ind in index if series.loc[ind] != 0
-                ])
-            ]
-        )
-    else:
-        return None
+        return {'row': index, 'column': 0}
+    return None
 
-app.layout = html.Div([
-    html.H1(
-        'Skillshare Churn Predictions',
-        style={
-            'textAlign': 'center',
-            'color': 'black',
-            "background": "white"}),
-    html.P("\n\n"),
-    html.Div([
-        dcc.Graph(
-            id='graph-1',
-            figure=make_figure_1()),
-        html.Table(
-            id='table-1',
+app.layout = html.Div(
+    [
+        html.H1(
+            'Skillshare P1 Conversion Predictions',
             style={
-                "margin-left": "auto",
-                "margin-right": "auto"}
-        )],
-        style={
-            "margin": "0% 25% 0% 25%"
-        })])
+                'textAlign': 'center',
+                'color': 'black',
+                "background": "white"}
+        ),
+        html.P("\n\n", id="lb"),
+        html.Div(
+            [
+                dcc.Graph(
+                    id='graph-1',
+                    figure=make_figure_1()
+                ),
+            ],
+            style={
+                "margin": "0% 25% 0% 25%"
+            }
+        ),
+        dash_table.DataTable(
+            new_trialers.to_dict('records'), 
+            [{"name": i, "id": i} for i in new_trialers.columns],
+            id='table-1'
+        )
+    ]
+)
 
 app.run_server(debug=True)
